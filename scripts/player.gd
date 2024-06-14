2cabdd78ee63d6b02b6dfa93b289606fabd32703
Dcarreon <dcarreonr@gmail.com>
2024-06-14 17:20:57 -0500

    Implemented idle, run and placeholder jump animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,22 +4,35 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var animated_sprite = $AnimatedSprite2D
+
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED)	
+
+	# Direction = 1 signifies that the sprite is moving right. Direction = -1 sigifies that it's moving left
+	# By default the sprite faces right
+	if direction > 0:
+		animated_sprite.flip_h = false;
+	elif direction < 0:
+		animated_sprite.flip_h = true;
+
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
 
 	move_and_slide()
